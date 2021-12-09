1543a183d5ce66640945bb3366f41adba47ce042
jfsenechal <jfsenechal@gmail.com>
2021-12-09 21:42:42 +0100

    add title

--- src/components/IconesSocial.vue
+++ src/components/IconesSocial.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="flex">
-    <a class="icon inline-flex justify-center items-center" href="#">
+    <a class="icon inline-flex justify-center items-center" href="#" title="Page Facebook">
       <i class="fa fa-facebook"></i>
     </a>
-    <a class="icon inline-flex justify-center items-center" href="#">
+    <a class="icon inline-flex justify-center items-center" href="#" title="Page Twitter">
       <i class="fa fa-twitter"></i>
     </a>
-    <a class="icon inline-flex justify-center items-center" href="#">
-      <i class="fa fa-linkedin"></i>
+    <a class="icon inline-flex justify-center items-center" href="#" title="Page Instagram">
+      <i class="fa fa-instagram"></i>
     </a>
-    <a class="icon inline-flex justify-center items-center" href="#">
+    <a class="icon inline-flex justify-center items-center" href="#" title="Page Youtube">
       <i class="fa fa-youtube"></i>
     </a>
   </div>
