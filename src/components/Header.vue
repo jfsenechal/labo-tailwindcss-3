7184e8967401ef4e263f0519fdb0d9a09a2662ac
jfsenechal <jfsenechal@gmail.com>
2021-12-12 20:56:22 +0100

    version between

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,12 +4,12 @@ import IconesSocial from "./IconesSocial.vue";
 </script>
 
 <template>
-  <header class="flex justify-end items-center p-3">
-    <a href="#">
-      <img class="w-85 h-50" alt="logo"
+  <header class="flex justify-between items-center p-3">
+    <a href="#" class="flex flex-grow flex-shrink-0">
+      <img class="w-85 h-50 inline" alt="logo"
            src="https://www.marche.be/wp-content/themes/marchebe/assets/tartine/rsc/img/img_logo.png"/>
     </a>
-    <nav class="ml-auto">
+    <nav class="ml-au45to">
       <Navigation/>
     </nav>
     <IconesSocial/>
